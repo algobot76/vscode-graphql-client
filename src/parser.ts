c154d418fb44838db760fa495e1df0ea6cdc6168
Kaitian Xie <xkaitian@gmail.com>
2019-07-16 16:43:11 -0700

    Extract api from selected text

--- src/parser.ts
+++ src/parser.ts
@@ -22,5 +22,29 @@ export class Parser {
         if (lines.length === 0) {
             throw Error('Selected text cannot be empty');
         }
+
+        // Get api from text
+        let api;
+        try {
+            api = this.getApi(lines[0]);
+        } catch (err) {
+            throw new Error(err.message);
+        }
+    }
+
+    private static getApi(line: string): string {
+        // `line` has the format of `POST anAPI`
+        // e.g. `POST https://spotify-graphql-server.herokuapp.com/graphql`
+        line = line.trim();
+        let firstWord = line.slice(0, 4);
+        if (firstWord.toUpperCase() !== 'POST') {
+            throw new Error('The first line must start with \'Post\'');
+        }
+        line = line.slice(5);
+        line = line.trimLeft();
+        if (line === '') {
+            throw new Error('API must be specified');
+        }
+        return line;
     }
 }
