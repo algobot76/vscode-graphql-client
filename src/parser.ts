5392b999736d7a6edcfabb2464a290e5b9cf0ff6
Kaitian Xie <xkaitian@gmail.com>
2019-07-16 16:16:15 -0700

    Break text into multiple lines

--- src/parser.ts
+++ src/parser.ts
@@ -1,5 +1,26 @@
-export class Parser {
-    public static parse(text: String) {
+import { EOL } from 'os';
+import { GraphqlRequest } from './models/GraphqlRequest';
 
+export class Parser {
+    public static parse(text: String): GraphqlRequest | undefined {
+        // Break text into multiple lines
+        let lines: string[] = text.split(EOL);
+        let idx = 0;
+        for (let i = 0; i < lines.length; i++) {
+            if (lines[i].trim() === '') {
+                if (i === lines.length - 1) {
+                    idx = i;
+                    break;
+                }
+                if (lines[i + 1] !== '') {
+                    idx = i;
+                    break;
+                }
+            }
+        }
+        lines = lines.splice(idx + 1);
+        if (lines.length === 0) {
+            return undefined;
+        }
     }
 }
