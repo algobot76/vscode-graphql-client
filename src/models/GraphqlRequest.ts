f778d9c82ea4ca1fbb785a3b17c3b63392857709
kyo-tang <kyo.hideki.tang@gmail.com>
2019-07-19 22:32:25 -0700

    added GraphqlRequest field

--- src/models/GraphqlRequest.ts
+++ src/models/GraphqlRequest.ts
@@ -1,9 +1,11 @@
 export class GraphqlRequest {
     api: string;
     query: string;
+    variables: any;
 
-    constructor(api: string, query: string) {
+    constructor(api: string, query: string, variables = {}) {
         this.api = api;
         this.query = query;
+        this.variables = variables;
     }
 }
